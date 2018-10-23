0c3d2379d83484fc18ba17da92131d99bdcb175e
pampajcs <jcscatuerchio@gmail.com>
2018-10-23 15:21:22 -0300

    Add files via upload

--- tp1/main.c
+++ tp1/main.c
@@ -1,4 +1,4 @@
-//----Programa TP1----
+/*----Programa TP1----
 //Descripcion: Simulacion de funcionamiento
 //             de un router de cuatro bocas,
 //             cada una de ellas conectadas
@@ -6,13 +6,14 @@
 //Materia: Sistemas operativos y redes
 //Profesor: Gerardo Sager
 //Autores: Scatuerchio Juan Cruz
-//         Aparicio Facundo
+//         Aparicio Facundo*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
 
 /*----VARIABLES----*/
+
 struct boca{
     char ip[16];
     char subnet[16];
@@ -24,46 +25,69 @@ struct boca bocaB;
 struct boca bocaC;
 struct boca bocaD;
 char def_gateway[16];
+char ip_privada[]="192.168.0.255";
 
 struct pack{
     char ip_origen[16];
     char ip_destino[16];
-    char boca_in[16];
-    char boca_out[16];
-    char default_gateway[3];
+    int fin_archivo;
 };
 
 struct pack packA;
 struct pack packB;
 struct pack packC;
 struct pack packD;
+struct in_addr *addr;
 
 FILE *config,*pack_bocaA,*pack_bocaB,*pack_bocaC,*pack_bocaD,*rechazados,*decisiones_qd,*decisiones_hexa;
 
+    unsigned long dir;
+    char default_gateway[]="no";
+
 /*----FUNCIONES----*/
+
 int get_config(FILE * archivo);
 int verificacion(struct boca boca_n);
 struct pack read_pack(FILE * pack_boca);
+void out(struct pack paquetes);
+unsigned long identif_boca(unsigned long ip_origen_);
 
 /*----PROGRAMA----*/
+
 int main(){
     config = fopen("routerconfig.txt","r");
     pack_bocaA = fopen("bocaA.txt","r");
-    pack_bocaD = fopen("decisiones_qd.txt","r");
     pack_bocaB = fopen("bocaB.txt","r");
     pack_bocaC = fopen("bocaC.txt","r");
     pack_bocaD = fopen("bocaD.txt","r");
-    rechazados = fopen("rechazados.txt","rw");
-    decisiones_qd = fopen("decisiones_qd.txt","rw");
-    decisiones_hexa = fopen("decisiones_hexa.txt","rw");
+    rechazados = fopen("rechazados.txt","w");
+    decisiones_qd = fopen("decisiones_qd.txt","w");
+    decisiones_hexa = fopen("decisiones_hexa.txt","w");
     if( config == NULL ) {
         printf("No se encontro el archivo de configuracion");
     }
     get_config(config);
-    packA = read_pack(pack_bocaA);
-    packB = read_pack(pack_bocaB);
-    packC = read_pack(pack_bocaC);
-    packD = read_pack(pack_bocaD);
+    fprintf(decisiones_qd,"IP origen        IP destino      Boca IN     Boca OUT        DG\n");
+    printf("boca A\n");
+    while(packA.fin_archivo==0){
+        packA = read_pack(pack_bocaA);
+        out(packA);
+    }
+    printf("boca B\n");
+    while(packB.fin_archivo==0){
+        packB = read_pack(pack_bocaB);
+        out(packB);
+    }
+    printf("boca C\n");
+    while(packC.fin_archivo==0){
+        packC = read_pack(pack_bocaC);
+        out(packC);
+    }
+    printf("boca D\n");
+    while(packD.fin_archivo==0){
+        packD = read_pack(pack_bocaD);
+        out(packD);
+    }
     return 0;
 }
 
@@ -82,31 +106,93 @@ int get_config(FILE * file){
     fscanf(file,"%s",bocaD.subnet);
     fscanf(file,"%s",bocaD.mask);
     fscanf(file,"%s",def_gateway);
-
-   /*printf("%s\n",bocaA.ip);
-    printf("%s\n",bocaA.mask);
-    printf("%s\n",bocaB.ip);inet_addr(bocaA.ip)
-    printf("%s\n",bocaB.mask);
-    printf("%s\n",bocaC.ip);
-    printf("%s\n",bocaC.mask);
-    printf("%s\n",bocaD.ip);
-    printf("%s\n",bocaD.mask);
-    printf("%s\n",def_gateway);*/
     return error;
 }
 
 int verificacion(struct boca boca_n){
-    //hacer comparacion de la salida de la and entre ip y mask con subred
+    /*hacer comparacion de la salida de la and entre ip y mask con subred*/
+
+
+
+    /*Lo hace Facu??*/
     return 0;
 }
 
-struct pack read_pack(FILE * pack_boca ){
+struct pack read_pack(FILE * pack_boca ){           /*funcion que lee el archivo de paquetes*/
     struct pack salida;
-    fscanf(pack_boca,"%s",salida.ip_origen);
-    fscanf(pack_boca,"%s",salida.ip_destino);
-    fscanf(pack_boca,"%s",salida.boca_in);
-    fscanf(pack_boca,"%s",salida.boca_out);
-    fscanf(pack_boca,"%s",salida.default_gateway);
+    if(!feof(pack_boca)){
+        fscanf(pack_boca,"%s",salida.ip_origen);
+        fscanf(pack_boca,"%s",salida.ip_destino);
+        salida.fin_archivo=0;
+    }
+    else{
+        salida.fin_archivo=1;
+    }
     return salida;
+}
+
+void out(struct pack paquetes){             /*funcion que escribe en el archivo de decisiones*/
+    if(paquetes.fin_archivo==0){
+        unsigned long ip_or=ntohl(inet_addr(paquetes.ip_origen));
+        unsigned long ip_des=ntohl(inet_addr(paquetes.ip_destino));
+        unsigned long boca_en=identif_boca(ip_or);
+        unsigned long boca_sal=identif_boca(ip_des);
+        if(boca_en == 0 || boca_sal==0){
+            printf("rechazado\n");
+            fprintf(rechazados,"%X %X %s %s\n",ip_or,ip_des,paquetes.ip_origen,paquetes.ip_destino);
+        }
+        else{
+            printf("%X  %X  %X  %X  %s\n", ip_or,ip_des,boca_en, boca_sal,default_gateway);
+            fprintf(decisiones_hexa,"%X %X  %X      %X      %s\n", ip_or,ip_des,boca_en, boca_sal,default_gateway);
+            fprintf(decisiones_qd,"%s   %s    %X    %X     %s\n", paquetes.ip_origen,paquetes.ip_destino,boca_en, boca_sal,default_gateway);
+        }
+    }
+}
+
+unsigned long identif_boca(unsigned long ip_origen_dest){       /*funcion que identifica la boca de ip de origen o destino*/
+    unsigned long identif_boca_sal;
+    if(ip_origen_dest==ntohl(inet_addr(ip_privada))){
+        identif_boca_sal=0;
+        printf("salida = 0");
+    }
+    else{
+        if((ip_origen_dest & ntohl(inet_addr(bocaA.mask))) == ntohl(inet_addr(bocaA.subnet))){
+            identif_boca_sal=ntohl(inet_addr(bocaA.ip));
+        }
+        else{
+            if((ip_origen_dest & ntohl(inet_addr(bocaB.mask))) == ntohl(inet_addr(bocaB.subnet))){
+                identif_boca_sal=ntohl(inet_addr(bocaB.ip));
+            }
+            else{
+                if((ip_origen_dest & ntohl(inet_addr(bocaC.mask))) == ntohl(inet_addr(bocaC.subnet))){
+                    identif_boca_sal=ntohl(inet_addr(bocaC.ip));
+                }
+                else{
+                    if((ip_origen_dest & ntohl(inet_addr(bocaD.mask))) == ntohl(inet_addr(bocaD.subnet))){
+                        identif_boca_sal=ntohl(inet_addr(bocaD.ip));
+                    }
+                    else{
+                        if(ip_origen_dest==ntohl(inet_addr(ip_privada))){
+                            identif_boca_sal=0;
+                        }
+                        else{
+                            /*identif_boca_sal = boca del default gateway
+                            default_gateway[0]='s';
+                            default_gateway[1]='i';*/
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if(ip_origen_dest == ntohl(inet_addr(def_gateway))){
+        default_gateway[0]='s';
+        default_gateway[1]='i';
+    }
+    else{
+        default_gateway[0]='n';
+        default_gateway[1]='o';
+    }
+    return identif_boca_sal;
 }
 
