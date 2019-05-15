d5df919ce2359a34d32fc9dd81fba1a8c0b6aaa9
pampajcs <jcscatuerchio@gmail.com>
2019-05-15 15:27:43 -0300

    Add files via upload

--- tp4.c
+++ tp4.c
@@ -17,9 +17,11 @@ int maso2[40];
 int carta=0;
 int i=0;
 int j=0;
+int cant_jug=4;
+int cant_manos=0;
 
 struct Jugador{
-	int mano[3];
+	int mano[18][3];	//primer indice= n° de mano, segundo indice= cartas de cada mano
 	int pila[40];
 };
 
@@ -27,12 +29,49 @@ struct Flop{
 	int cartas_mesa[40];
 };
 
-int main(){ 
+struct Jugador P1;
+struct Jugador P2;
+struct Jugador P3;
+struct Jugador P4;
+struct Flop flop;
+
+
+int main(){
 	printf("ESCOBA DE 15\n");  
 	printf("\n");
+	cant_manos=12/(cant_jug);
 	barajar();
 	manos();
+	for(i=0;i<cant_manos;i++){
+		for(j=0;j<3;j++){
+			interprete_carta(P1.mano[i][j]);
+		}
+		printf("\n");	
+	}
+	printf("\n");
+	for(i=0;i<cant_manos;i++){
+		for(j=0;j<3;j++){
+			interprete_carta(P2.mano[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	for(i=0;i<cant_manos;i++){
+		for(j=0;j<3;j++){
+			interprete_carta(P3.mano[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	for(i=0;i<cant_manos;i++){
+		for(j=0;j<3;j++){
+			interprete_carta(P4.mano[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
 	while(1){
+	
 	}
 }
 
@@ -98,23 +137,16 @@ void interprete_carta(int card){		/*a cada posicion del maso lo interpreto como 
 }
 
 void manos(void){
-	struct Jugador P1;
-	struct Jugador P2;
-	struct Jugador P3;
-	struct Jugador P4;
-	struct Flop flop;
-	int cant_jug=3;
-	int cant_manos=12/(cant_jug);
 	printf("FLOP\n");
 	for(i=0;i<4;i++){
 		flop.cartas_mesa[i]=maso2[i];
 		interprete_carta(flop.cartas_mesa[i]);
-		printf("	");
+		//printf("	");
 	}	
 	printf("\n");
 	printf("\n");
 	for(i=0;i<cant_manos;i++){
-		switch(cant_jug){
+		/*switch(cant_jug){
 			case 2:
 				printf("P1	P2	P1	P2	P1	P2	Mano %d\n",i+1);
 				break;
@@ -124,29 +156,45 @@ void manos(void){
 			case 4:
 				printf("P1	P2	P3	P4	P1	P2	P3	P4	P1	P2	P3	P4	Mano %d\n",i+1);
 				break;
-		}
+		}*/
 		//printf("P1	P2	P1	P2	P1	P2	Mano %d\n",i+1);
-		for(j=0;j<3*cant_jug;j++){
-			switch(j%cant_jug){
-				case 0:
-					P1.mano[j]=maso2[j+4+3*cant_jug*i];
-					interprete_carta(P1.mano[j]);
-					break;
-				case 1:
-					printf("	");
-					P2.mano[j]=maso2[j+4+3*cant_jug*i];
-					interprete_carta(P2.mano[j]);
-					//printf("	");
-					break;
+		for(j=0;j<3;j++){//3*cant_jug;j++){
+			P1.mano[i][j]=maso2[j+4+3*cant_jug*i];
+			P2.mano[i][j]=maso2[j+5+3*cant_jug*i];
+			P3.mano[i][j]=maso2[j+6+3*cant_jug*i];
+			//P4.mano[i][j]=maso2[j+7+3*cant_jug*i];
+
+			switch(cant_jug){
 				case 2:
-					printf("	");
-					P3.mano[j]=maso2[j+4+3*cant_jug*i];
-					interprete_carta(P3.mano[j]);
-					printf("	");
+					P1.mano[i][j]=maso2[j+4+3*cant_jug*i];
+					P2.mano[i][j]=maso2[j+5+3*cant_jug*i];
 					break;
 				case 3:
-					printf("	");
-					P4.mano[j]=maso2[j+4+3*cant_jug*i];
-					interprete_carta(P4.mano[j]);
-					printf("	");
+					P1.mano[i][j]=maso2[j+4+3*cant_jug*i];
+					P2.mano[i][j]=maso2[j+5+3*cant_jug*i];
+					P3.mano[i][j]=maso2[j+6+3*cant_jug*i];
 					break;
+				case 4:
+					P1.mano[i][j]=maso2[j+4+3*cant_jug*i];
+					P2.mano[i][j]=maso2[j+5+3*cant_jug*i];
+					P3.mano[i][j]=maso2[j+6+3*cant_jug*i];
+					P4.mano[i][j]=maso2[j+7+3*cant_jug*i];
+				break;
+			}
+			/*if(j%cant_jug==0){
+				P1.mano[j]=maso2[j+4+3*cant_jug*i];
+				interprete_carta(P1.mano[j]);
+				//printf("%d	",P1.mano[j]);
+			}
+			if(j%2==1){
+				printf("	");
+				P2.mano[j]=maso2[j+4+3*cant_jug*i];
+				interprete_carta(P2.mano[j]);
+				printf("	");
+				//printf("%d ",P2.mano[j+4]);
+			}*/
+		}
+		//printf("\n");
+		//printf("\n");
+	}
+}
